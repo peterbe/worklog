2b991f18956b6ea5886abf4061fb1a058a405d17
Peter Bengtsson <peter@fry-it.com>
2010-10-17 17:12:37 +0100

    modified _run_tests.py

--- _run_tests.py
+++ _run_tests.py
@@ -9,7 +9,15 @@ TEST_MODULES = [
 ]
 
 def all():
-    return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)
+    try:
+        return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)
+    except AttributeError, e:
+        if "'module' object has no attribute 'test_app'" in str(e):
+            # most likely because of an import error
+            for m in TEST_MODULES:
+                __import__(m, globals(), locals())
+        raise
+        
 
 if __name__ == '__main__':
     import tornado.testing
