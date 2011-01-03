bb32688339c88e244a7718dcb2408d4b65ea137c
Peter Bengtsson <peter@fry-it.com>
2011-01-03 14:19:37 +0100

    adding a new script

---
+++ download_static_urls.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from time import time
+import lxml.html
+from lxml import etree
+from lxml.cssselect import CSSSelector
+from urllib import urlopen
+from urlparse import urlparse
+
+def wrap(url, uri):
+    if uri.startswith('/'):
+        parts = list(urlparse(url))
+        parts[2] = uri
+        url = parts[0]
+        url += '://'
+        if parts[2].startswith('//'):
+            url += parts[2][2:]
+        else:
+            url += parts[1]
+            url += parts[2]
+    return url
+
+def get_static_urls(url):
+    html = urlopen(url).read()
+    parser = etree.HTMLParser()
+    tree = etree.fromstring(html.strip(), parser).getroottree()
+    page = tree.getroot()
+    for link in CSSSelector('link')(page):
+        yield wrap(url, link.attrib['href'])
+        
+    
+def main(*args):
+    for arg in args:
+        for url in get_static_urls(arg):
+            t0=time()
+            content = urlopen(url).read()
+            t1=time()
+            t = round((t1-t0) * 1000, 2)
+            print ("%s Kb" % (len(content)/1024)).ljust(10),
+            print ("%sms"%t).ljust(10),
+            print url#, "%sms"%t, "%s Kb" % (len(content)/1024)
+    return 0
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(*sys.argv[1:]))
